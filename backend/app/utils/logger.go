1f9f1c42a4bafef462e63ad4ea7d568933b70c91
Heyder <heyderpd@gmail.com>
2024-12-05 21:21:21 +0000

    create Log setup by env

---
+++ backend/app/utils/logger.go
@@ -0,0 +1,29 @@
+package utils
+
+import (
+	"os"
+	"strings"
+
+	"github.com/sirupsen/logrus"
+)
+
+var Log *logrus.Logger
+
+var (
+	logModes = map[string]logrus.Level {
+		"debug": logrus.DebugLevel,
+		"info": logrus.InfoLevel,
+		"error": logrus.ErrorLevel,
+	}
+	defaultLogMode = logrus.ErrorLevel
+)
+
+func LoggerInit() {
+	var LogLevelStr = strings.ToLower(os.Getenv("LOGLEVEL"))
+	var logLevel, found = logModes[LogLevelStr]
+	if !found {
+		logLevel = defaultLogMode
+	}
+	Log = logrus.New()
+	Log.SetLevel(logLevel)
+}
