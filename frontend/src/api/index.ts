3c4ac323dee94f2743bd840fa6d20589955574a6
Heyder <heyderpd@gmail.com>
2024-12-08 17:57:53 +0000

    create axios instance

--- frontend/src/api/index.ts
+++ frontend/src/api/index.ts
@@ -8,11 +8,18 @@ interface Result {
   value: string;
 }
 
+const Api = axios.create({
+  baseURL: BASE_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+})
+
 export async function findIndex(value: string): Promise<Result | null> {
   try {
-    const response = await axios.get(`${BASE_URL}/endpoint/${value}`);
+    const response = await Api.get(`${BASE_URL}/endpoint/${value}`);
     const data = await response.data as Result;
-    console.debug(response.status, data)
+    console.debug(response?.status, response)
     return { ...data, status: response.status.toString() };
   } catch (err) {
     console.error(err)
